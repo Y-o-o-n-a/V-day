667c69c03ea8a1af0f3f5e5c7555772b8168bec0
Papi <93372757+Y-o-o-n-a@users.noreply.github.com>
2026-02-08 12:47:07 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ function App() {
               className="mb-6"
             >
               <img
-                src="https://tenor.com/view/bubu-dudu-rose-dudu-propose-dudu-gives-rose-dudu-giving-rose-rose-gif-7052649493461014161"
+                src="https://tenor.com/view/love-gif-7644330066038750623"
                 alt="Cute bear with flowers"
                 className="w-32 h-32 sm:w-40 sm:h-40 mx-auto object-contain"
               />
